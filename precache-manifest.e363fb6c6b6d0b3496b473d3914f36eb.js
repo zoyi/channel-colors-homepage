a3b4220b36c0a07e43c510b322db00c423176d08
Junbong Lee <koliie9039@gmail.com>
2019-09-09 17:06:44 +0900

    Updates

---
+++ precache-manifest.e363fb6c6b6d0b3496b473d3914f36eb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abda776a91e1e5734377d1340578f246",
+    "url": "/channel-colors-homepage/index.html"
+  },
+  {
+    "revision": "79b2327803e85fbaafe1",
+    "url": "/channel-colors-homepage/static/css/main.1f224d7d.chunk.css"
+  },
+  {
+    "revision": "cbfb84f0fc13ab368591",
+    "url": "/channel-colors-homepage/static/js/2.842844fc.chunk.js"
+  },
+  {
+    "revision": "79b2327803e85fbaafe1",
+    "url": "/channel-colors-homepage/static/js/main.3dcf1201.chunk.js"
+  },
+  {
+    "revision": "3da792c0da66b2e6cded",
+    "url": "/channel-colors-homepage/static/js/runtime~main.e548b269.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/channel-colors-homepage/precache-manifest.f43e471e0f60044ee38f687c54deb1ad.js"
+  "/channel-colors-homepage/precache-manifest.e363fb6c6b6d0b3496b473d3914f36eb.js"
 );
 
 self.addEventListener('message', (event) => {
