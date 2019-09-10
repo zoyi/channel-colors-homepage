8b5b0e0ca8260c39d042cc16b3a049607b841b7d
Junbong Lee <koliie9039@gmail.com>
2019-09-10 12:19:51 +0900

    Update style of Swatch component.

--- src/components/Swatch/Swatch.jsx
+++ src/components/Swatch/Swatch.jsx
@@ -8,10 +8,14 @@ function Swatch({ name, color }) {
   return (
     <div className={styles.Swatch}>
       { (opacity < 100) && (<div className={styles.Backer} />) }
+
       <div
         className={styles.Color}
         style={{ backgroundColor: hex, opacity: _.round(opacity / 100, 2) }}
       />
+
+      <div className={styles.ColorBorder} />
+
       <div className={styles.Label}>
         <div>{ name }</div>
         <div className={styles.Hex}>{ hex }</div>
