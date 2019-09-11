94b429f90ded926c5a00d0e857929450e90c7c2c
Junbong Lee <koliie9039@gmail.com>
2019-09-12 00:06:47 +0900

    Add code usages.

--- src/components/Content/sections/Downloads/Downloads.jsx
+++ src/components/Content/sections/Downloads/Downloads.jsx
@@ -1,21 +1,27 @@
 import React from 'react';
 
+import { SCSS, CSS, ANDROID_XML, JSON_SCHEME } from '../../../../resources/etc/usages';
+
 function Downloads() {
   return (
     <section id="downloads">
-      <h1>Downloads</h1>
+      <h1>Downloads / Usage</h1>
 
       <h2>SCSS</h2>
       <a href="http://cdn.channel.io/colors/default.colors.scss" download>Download</a>
+      <code>{ SCSS }</code>
 
       <h2>CSS</h2>
       <a href="http://cdn.channel.io/colors/default.colors.css" download>Download</a>
+      <code>{ CSS }</code>
 
       <h2>Android XML</h2>
       <a href="http://cdn.channel.io/colors/android.default.colors.xml" download>Download</a>
+      <code>{ ANDROID_XML }</code>
 
       <h2>JSON</h2>
       <a href="http://cdn.channel.io/colors/default.colors.json" download>Download</a>
+      <code>{ JSON_SCHEME }</code>
     </section>
   )
 }
---
+++ src/resources/etc/usages.js
@@ -0,0 +1,20 @@
+export const SCSS = `div {
+  background-color: $ch-navy600;
+}`;
+
+export const CSS = `div {
+  background-color: var(--navy-600);
+}`;
+
+export const ANDROID_XML = `<TextView
+  android:background="@color/navy600" />`;
+
+export const JSON_SCHEME = `{
+  "navy600": {
+    "hex": "#09124E",
+    "opacity": 100,
+    "rgb": "9,18,78",
+    "family": "navy"
+  },
+  ...
+}`;
